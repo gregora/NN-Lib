0acd78d8c2323d7146dfef89097cdbafb98d2937
Yon Ploj <yon.ploj@gmail.com>
2021-06-02 18:49:45 +0200

    designed a basic network.h framework

--- include/misc.h
+++ include/misc.h
@@ -6,5 +6,14 @@ namespace nnlib {
 	// uniformly distributed random float between 0 and 1
 	float random();
 	float random(float min, float max);
-	
+
+	float sigmoid(float x);
+
+	float sparseCategoricalCrossentropy(float* predicted, float truth);
+
+	float categoricalCrossentropy(float* predicted, float* truth);
+
+	float binaryCrossentropy(float* predicted, float* truth);
+
+	float meanSquaredError(float* predicted, float* truth);
 }
--- include/network.h
+++ include/network.h
@@ -3,5 +3,49 @@
 
 namespace nnlib {
 
+	// abstract Layer class: not to be implemented
+	class Layer {
+	public:
+		virtual std::string toString() = 0;
 
+		virtual std::string serialize() = 0;
+		virtual std::string deserialize() = 0;
+
+		virtual float eval(uint datac, float* datav);
+
+	};
+
+	class Dense: public Layer {
+	public:
+		std::string toString();
+
+		std::string serialize();
+		std::string deserialize();
+
+	};
+
+	class Network {
+	public:
+		Network();
+		~Network();
+
+		void addLayer(Layer* l);
+
+		float eval(uint datac, float* datav);
+
+		void save(std::string path);
+		void load(std::string path);
+
+		std::string serialize();
+		std::string deserialize();
+
+		// pretty-print; not for exporting
+		std::string toString();
+
+		Network clone();
+
+	private:
+		Layer* layers;
+
+	};
 }

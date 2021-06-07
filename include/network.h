40bac7a8b1fcec571fc063335e468285faa3f193
Gregor A <gregor.alic@gimb.org>
2021-06-07 21:58:41 +0200

    Fixes  on network.h file

--- include/network.h
+++ include/network.h
@@ -9,7 +9,7 @@ namespace nnlib {
 		virtual std::string toString() = 0;
 
 		virtual std::string serialize() = 0;
-		virtual void deserialize() = 0;
+		virtual void deserialize(std::string input) = 0;
 
 		virtual float eval(Matrix* input, Matrix* output);
 
@@ -22,7 +22,7 @@ namespace nnlib {
 		std::string toString();
 
 		std::string serialize();
-		void deserialize();
+		void deserialize(std::string input);
 
 		Matrix* matrix;
 
@@ -41,7 +41,7 @@ namespace nnlib {
 		void load(std::string path);
 
 		std::string serialize();
-		void deserialize();
+		void deserialize(std::string input);
 
 		// pretty-print; not for exporting
 		std::string toString();
